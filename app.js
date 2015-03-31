ef487045187aa7aad4aa74b7ace1e4b598c3e5ce
404pnf <404pnf@exmaple.org>
2015-03-31 11:01:17 +0800

    v1.0

---
+++ app.js
@@ -0,0 +1,31 @@
+var fs = require('fs'),
+    express = require('express'),
+    wkhtmltopdf = require('wkhtmltopdf'),
+    uuid = require('node-uuid'),
+    app = express();
+
+// ## 安装wkhtmltopdf
+// http://wkhtmltopdf.org/
+// ## 如何使用wkhtmltopdf
+// https://github.com/devongovett/node-wkhtmltopdf
+// 使用方法
+// // Optional callback
+// wkhtmltopdf('http://google.com/', { pageSize: 'letter' }, function (code, signal) {
+// });
+
+app.get('/', function (req, res) {
+    var s = '<meta charset="utf-8"><h1>呼呼haha</h1>',
+        filename = uuid.v4(),
+        options = {
+            output: filename,
+            footerCenter: "页脚也可以用中文",
+            pageSize: "A4",
+        };
+    wkhtmltopdf(s, options, function (code, signal) {
+        res.download(filename, "huhu.pdf");
+    });
+});
+
+app.listen(3000, function () {
+    console.log("port 3000.");
+});
